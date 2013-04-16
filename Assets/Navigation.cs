89a87899cf75acb07f9dd8c560f6a7cce49c65ea
tbporter <travis.porter@gmail.com>
2013-04-16 07:05:25 -0400

    Added memory visualization. Slight movement tweak. Got rid of some movement code

--- Assets/Navigation.cs
+++ Assets/Navigation.cs
@@ -14,10 +14,23 @@ public class Navigation : MonoBehaviour {
 	private Encoder LeftEncoder;
 	private Encoder RightEncoder;
 	
-	private float[] leftMem;
-	private float[] rightMem;
+	private memory[] mem;
+	private memoryObject[] memObj;
 	
 	private float[] turns;
+	
+	
+	struct memory{
+		public float IRLeft;
+		public float IRRight;
+		public Vector3 dir;
+	}
+	private int lastMem;
+	struct memoryObject{
+		public GameObject left;
+		public GameObject right;
+		public GameObject robot;
+	}
 	
 	private float corner;
 	enum state{
@@ -43,9 +56,8 @@ public class Navigation : MonoBehaviour {
 		IRSideBackLeft = new IR(transform.FindChild("left_side_back_IR"));
 		IRSideBackRight= new IR(transform.FindChild("right_side_back_IR"));
 		
-		leftMem = new float[1000];
-		rightMem = new float[1000];
-		
+		mem = new memory[1000];
+		memObj = new memoryObject[1000];
 		LeftEncoder = new Encoder(transform.FindChild ("left_front_wheel"));
 		RightEncoder = new Encoder(transform.FindChild ("right_front_wheel"));
 		
@@ -65,10 +77,13 @@ public class Navigation : MonoBehaviour {
 		RightEncoder.update ();
 	}
 	void FixedUpdate () {
-		leftMem[(int)LeftEncoder.getDistance()]=IRSideFrontLeft.getDistance();
-		rightMem[(int)RightEncoder.getDistance ()]=IRSideFrontRight.getDistance();
-		print (RightEncoder.getDistance());
+		int temp = (int)RightEncoder.getDistance ();
+		mem[temp].IRLeft = IRSideFrontLeft.getDistance();
+		mem[temp].IRRight = IRSideFrontRight.getDistance();
+		mem[temp].dir =  transform.forward;
+		lastMem = temp;
 		stateMachine ();
+		UserControl();
 	}
 	
 	
@@ -132,73 +147,14 @@ public class Navigation : MonoBehaviour {
 			}
 			
 			break;
-			
-		case state.side:
-			if(checkParallel(curDir)){
-				curDir = dir.straight;
-				curState= state.straight;
-				break;
-			}
-			if(curDir==dir.right){
-				IRSideBackRight.debug (Color.red);
-				IRSideFrontRight.debug (Color.red);
-				servoControl (.1f,2f);
-			}
-			else{
-				IRSideBackLeft.debug (Color.red);
-				IRSideFrontLeft.debug (Color.red);
-				servoControl (2f,.1f);
-			}
-			break;
 		}
-	}
-	/* It says checkParallel, but really its to make sure the side-front IR 
-	 * isn't pointing close to the wall than the side-back
-	 * input: direction of IRs wanting to check
-	 * output: whether it is parallel*/
-	bool checkParallel(dir d){
-		const float PARALLEL_DIFF_MIN = .15f;
-		float front;
-		float back;
-		if(d == dir.right){
-			front =IRSideFrontRight.getDistance ();
-			back =IRSideBackRight.getDistance ();
-		}
-		else{
-			front =IRSideFrontLeft.getDistance ();
-			back =IRSideBackLeft.getDistance ();
-		}
-		if(front+PARALLEL_DIFF_MIN<back){
-			return false;
-		}
-			return true;
 	}
 	
 	dir checkStraight(){
 
 		float front_diff= Mathf.Abs(IRSideFrontRight.getDistance()-IRSideFrontLeft.getDistance());
-		float left_side_diff= Mathf.Abs(IRSideFrontLeft.getDistance()-IRSideBackLeft.getDistance());
-		float right_side_diff= Mathf.Abs(IRSideFrontRight.getDistance()-IRSideBackRight.getDistance());
-		/*if(front_diff>1f){
-			if(IRSideFrontRight.getDistance ()>IRSideFrontLeft.getDistance ()){
-				if(right_side_diff>.04f){
-					print ("right side");
-					servoControl (2f,1.5f);
-					return dir.left;
-				}
-				
-			}
-			else{
-				if(left_side_diff>.04f){
-					
-					print ("left side");
-					servoControl (1.5f,2f);
-					return dir.right;
-				}
-
-			}
-		}*/
-		if(IRSideFrontLeft.getDistance()<5){
+		
+		if(IRSideFrontLeft.getDistance()<5 & front_diff>1){
 			if(IRSideFrontLeft.getDistance()<4){
 				servoControl (0f,-2f);
 				return dir.left;
@@ -206,7 +162,7 @@ public class Navigation : MonoBehaviour {
 			servoControl (2f,1.5f);
 			return dir.left;
 		}
-		else if(IRSideFrontRight.getDistance()<5) {
+		else if(IRSideFrontRight.getDistance()<5 & front_diff>1) {
 			if(IRSideFrontRight.getDistance()<4){
 				servoControl (-2f,0f);
 				return dir.right;
@@ -219,9 +175,11 @@ public class Navigation : MonoBehaviour {
 			print ("lean to right");
 			servoControl (2f,1.98f);
 		}
-		else{
+		else if(IRSideFrontRight.getDistance ()<IRSideFrontLeft.getDistance ()){
 			print ("lean to left");
 			servoControl (1.98f,2f);
+		}else{
+			servoControl (2f,2f);
 		}
 		return dir.straight;
 	
@@ -253,40 +211,6 @@ public class Navigation : MonoBehaviour {
 		return dir.straight;
 	}
 
-	dir checkSide(){
-		
-		//we need to prioritize the direction we should turn by looking at which is closest
-		if(IRSideFrontRight.getDistance()<IRSideFrontLeft.getDistance()){
-			if(checkSideDir(dir.right))
-				return dir.right;
-			if(checkSideDir(dir.left))
-				return dir.left;
-		}
-		else{
-			if(checkSideDir(dir.left))
-				return dir.left;
-			if(checkSideDir(dir.right))
-				return dir.right;
-		}
-		return dir.straight;
-	}
-	/* Here we check to seeing if we are too close to the sides,
-	 * but if we are Parallel to the side, it's okay to be close
-	 * output: if the side should be turning*/
-	bool checkSideDir(dir d){
-		const float SIDE_IR_MIN = 3;
-		switch(d){
-		case dir.right:
-			if(!checkParallel(dir.right)&&IRSideBackRight.getDistance()<SIDE_IR_MIN && IRSideFrontRight.getDistance()<SIDE_IR_MIN)
-				return true;
-			break;
-		case dir.left:
-			if(!checkParallel(dir.left)&&IRSideBackLeft.getDistance()<SIDE_IR_MIN && IRSideFrontLeft.getDistance()<SIDE_IR_MIN)
-				return true;
-			break;
-		}
-		return false;
-	}
 	
 	void servoControl(float left, float right){
 
@@ -298,11 +222,11 @@ public class Navigation : MonoBehaviour {
 		
 		left = left*5;
 		right = right*5;
-		if(Input.GetButton("Horizontal")){
+		/*if(Input.GetButton("Horizontal")){
 			transform.rigidbody.velocity = Vector3.zero;
 			transform.rigidbody.angularVelocity = Vector3.zero;
 			return;
-		}
+		}*/
 		
 		Transform childT = transform.FindChild ("left_front_wheel");
 		transform.gameObject.rigidbody.AddForceAtPosition(transform.forward * left,childT.position);
@@ -317,26 +241,50 @@ public class Navigation : MonoBehaviour {
 	
 	
 	void UserControl(){
-		if(hori==0 && Input.GetButton("Horizontal"))
-			hori = Input.GetAxis ("Horizontal");
+		if(hori==0 && Input.GetButton("Horizontal")){
+			printMemory ();
+			hori = 1;
+		}
 		else
 			hori = 0;
 		
-		if(hori==0 && Input.GetButton("Vertical"))
-			vert = Input.GetAxis ("Vertical");
-		else
-			vert = 0;
-		
-		if(hori>0)
-			transform.Translate(1*50f*Time.deltaTime, 0f, 0f);
-		else if(hori<0)
-			transform.Translate(-1*50f*Time.deltaTime, 0f, 0f);
-		
-		
-		if(vert>0)
-			transform.Translate(0f, 0f, 1*50f*Time.deltaTime);
-		else if(vert<0)
-			transform.Translate(0f, 0f, -1*50f*Time.deltaTime);
+	}
+	
+	void printMemory(){
+		GameObject robot =  GameObject.CreatePrimitive(PrimitiveType.Cube);
+		robot.transform.position = new Vector3(27.90829f, 7.128446f, -25.02266f);
+		int i = 1;
+		print ("print Memory");
+		while(i<lastMem){
+			robot.transform.forward = mem[i].dir;
+			
+			Destroy(memObj[i].robot); 
+			memObj[i].robot = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			memObj[i].robot.transform.position = robot.transform.position;
+			memObj[i].robot.transform.renderer.material.SetColor ("_Color", Color.green);
+			
+			Destroy(memObj[i].left); 
+			memObj[i].left = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			memObj[i].left.transform.position = robot.transform.position;
+			memObj[i].left.transform.forward = robot.transform.forward;
+			memObj[i].left.transform.renderer.material.SetColor ("_Color", Color.red);
+			memObj[i].left.transform.Rotate (Vector3.up*308.6562f);
+			memObj[i].left.transform.Translate (Vector3.forward * mem[i].IRLeft);
+			
+			Destroy(memObj[i].right); 
+			memObj[i].right = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			memObj[i].right.transform.position = robot.transform.position;
+			memObj[i].right.transform.forward = robot.transform.forward;
+			memObj[i].right.transform.renderer.material.SetColor ("_Color", Color.blue);
+			memObj[i].right.transform.Rotate (Vector3.up*51.43085f);
+			memObj[i].right.transform.Translate (Vector3.forward * mem[i].IRRight);
+			
+			robot.transform.Translate (Vector3.forward * 1);
+			i++;
+		}
+		Destroy(robot);
+				//GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		//cube.transform.position = new Vector3(x, y, 0);
 	}
 }
 
@@ -382,6 +330,7 @@ class Encoder{
 	Transform transform;
 	public Encoder(Transform tran){
 		transform = tran;
+		prevPos = tran.position;
 	}
 	public void update(){
 		float temp = Vector3.Distance (transform.position, prevPos);
